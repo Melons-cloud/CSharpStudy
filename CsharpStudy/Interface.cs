adba73a8b635613a6e74d7e1fbaf1aaaefc62632
hjl19910901 <18710360575@163.com>
2019-07-23 11:42:47 +0800

    C#学习

---
+++ CsharpStudy/Interface.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace CsharpStudy
+{
+    interface IParentInterface
+    {
+        void ParentInterfaceMethod();
+    }
+
+    interface IMyInterface:IParentInterface
+    {
+        void MethodToImplement();
+    }
+     class InterfaceImplementer:IMyInterface
+    {
+         static void Main()
+        {
+            InterfaceImplementer implementer = new InterfaceImplementer();
+            implementer.MethodToImplement();
+            implementer.ParentInterfaceMethod();
+            
+            
+        }
+
+        public void ParentInterfaceMethod()
+        {
+           Console.WriteLine("MethodToImplement() called.");
+        }
+
+        public void MethodToImplement()
+        {
+           Console.WriteLine("ParentInterfaceMethod() called");
+        }
+    }
+}
