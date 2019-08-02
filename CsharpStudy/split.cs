435479b46e6b754e5b30a868e0c4288b2e0e6330
hjl19910901 <18710360575@163.com>
2019-08-02 11:36:54 +0800

    C#学习

--- CsharpStudy/split.cs
+++ CsharpStudy/split.cs
@@ -9,11 +9,11 @@ namespace CsharpStudy
             //声明字符串
             string str = "让编程学习不再难，让编程创造财富不再难，让编程改变工作和人生不再难";
             //声明分割字符的数组
-            char[] separater = { '.'};
+            char[] separater = {','};
             //分割字符串
             string[] splitStrings = str.Split(separater, StringSplitOptions.RemoveEmptyEntries);
             //使用for循环遍历数组，并输出
-            for (int i = 0; i < splitStrings.Length-1; i++)
+            for (int i = 0; i < splitStrings.Length; i++)
             {
                 Console.WriteLine(splitStrings[i]);
             }
