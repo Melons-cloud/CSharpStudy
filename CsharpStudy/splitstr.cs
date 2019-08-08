f02c69cdde0643e45ed08495af85dc44d5ce006a
hjl19910901 <18710360575@163.com>
2019-08-08 17:28:50 +0800

    C#学习

--- CsharpStudy/splitstr.cs
+++ CsharpStudy/splitstr.cs
@@ -4,7 +4,7 @@ namespace CsharpStudy
 {
     public class splitstr
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             //声明字符串
             string str = "让编程学习不再难，让编程创造财富不再难，让编程改变工作和人生不再难";
